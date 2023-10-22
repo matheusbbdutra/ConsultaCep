78ab55ee2bba4c5582380aca5510c5c3021e2f02
Matheus Dutra <matheusbbdutra@gmail.com>
2023-10-22 09:15:16 -0300

    [ADD]#2 Adicionando pesquisa de cep pelo logradouro

--- src/Controller/ConsultarCepController.php
+++ src/Controller/ConsultarCepController.php
@@ -22,14 +22,34 @@ class ConsultarCepController extends AbstractController
     }
 
     /**
+     * @param int $cep
+     * @return JsonResponse
      * @throws GuzzleException
      */
     #[Route(path: '/consulta-via-cep/{cep}', name: 'via_cep', methods: ['GET'])]
-    public function consultaCepAction(int $cep)
+    public function consultaCepAction(int $cep): JsonResponse
     {
 
         $request = $this->client->request('GET', "viacep.com.br/ws/{$cep}/json/");
         $contentsDecode = json_decode($request->getBody()->getContents());
+
+        return new JsonResponse($contentsDecode);
+    }
+
+    /**
+     * @param Request $request
+     * @return JsonResponse
+     * @throws GuzzleException
+     */
+    #[Route(path: '/pesquisar-cep', name: 'pesquisar_cep', methods: ['GET'])]
+    public function pesquisarCepAction(Request $request): JsonResponse
+    {
+        $uf = $request->query->get('uf');
+        $cidade = $request->query->get('cidade');
+        $logradouro = $request->query->get('logradouro');
+
+        $getRequest = $this->client->request('GET', "viacep.com.br/ws/{$uf}/{$cidade}/{$logradouro}/json/");
+        $contentsDecode = json_decode($getRequest->getBody()->getContents());
 
         return new JsonResponse($contentsDecode);
     }
